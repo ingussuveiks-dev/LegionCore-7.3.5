ba5dfbae785448b23868d6de18ecf5d6ed83da6c
ingussuveiks-dev <ingussuveiks@gmail.com>
2026-09-21 08:47:21 +0300

    Fix aura area trigger spell links

---
+++ sql/updates/world/2026_09_21_273_fix_aura_areatrigger_links.sql
@@ -0,0 +1,9 @@
+-- These templates are created by SPELL_AURA_CREATE_AREATRIGGER effects in
+-- the Legion 7.3.5 (26972) SpellEffect data, not by their obsolete spell IDs.
+UPDATE `areatrigger_template`
+SET `spellId` = 218680
+WHERE `entry` = 0 AND `customEntry` = 7782 AND `spellId` = 42989; -- Lava Waves
+
+UPDATE `areatrigger_template`
+SET `spellId` = 192258
+WHERE `entry` = 3681 AND `spellId` = 178965; -- Their Numbers Are Legion
